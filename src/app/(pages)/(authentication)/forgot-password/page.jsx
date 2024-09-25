705989111391e21fd0a8c6662cca9c57ffad8886
Naim Ahmad <naimahmad201103@gmail.com>
2024-09-25 11:23:20 +0600

    working on login page

---
+++ src/app/(pages)/(authentication)/forgot-password/page.jsx
@@ -0,0 +1,16 @@
+import ForgotPassword from "@/Components/Authentication/Forgot";
+import React from "react";
+export const metadata = {
+  title: "Forgot Passord || Digital Dropouts",
+  description: "",
+};
+
+const page = () => {
+  return (
+    <>
+      <ForgotPassword />
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/(pages)/(authentication)/register/page.jsx
@@ -0,0 +1,16 @@
+import Registration from "@/Components/Authentication/Registration";
+import React from "react";
+export const metadata = {
+  title: "Login || Digital Dropouts",
+  description: "",
+};
+
+const page = () => {
+  return (
+    <>
+      <Registration />
+    </>
+  );
+};
+
+export default page;
