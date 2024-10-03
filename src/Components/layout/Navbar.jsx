98db05e2179de1e7b4bcf6fb2c3314cb551c3aa8
Naim Ahmad <naimahmad201103@gmail.com>
2024-10-03 19:28:58 +0600

    added user logo and profile pic on navbar

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -2,11 +2,30 @@
 import logo from "@/assets/images/CodeSync_Station.png"
 import Image from "next/image"
 import Link from "next/link"
-import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { Dropdown } from "react-bootstrap"
+import { AiOutlineLogout } from "react-icons/ai"
+import { MdOutlineSpaceDashboard } from "react-icons/md"
+const Avatar = ({ url }) =>
+  url ? <Image src={url} alt="profile Image" /> : <svg style={{ width: "100%", height: "100%" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+  </svg>
+
 
 export default function Navbar() {
 
-  const activeNavbar = () => {
+  const router = useRouter()
+  const [userData, setUserData] = useState(null)
+ 
+  useEffect(() => {
+    getUserData()
+    activeNavbar()
+    window.addEventListener("scroll", activeNavbar);
+  }, [])
+
+  function activeNavbar() {
     // Get all the sections and the nav links
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll(".nav-link");
@@ -31,12 +50,22 @@ export default function Navbar() {
       }
     });
   }
-  useEffect(() => {
 
-    activeNavbar()
-    window.addEventListener("scroll", activeNavbar);
+  function getUserData() {
+    const userDataJson = localStorage.getItem("userData")
+    const data = typeof userDataJson === "string" ? JSON.parse(userDataJson) : null;
+    if (data) {
+      setUserData(data)
+    }else{
+      setUserData(null)
+    }
+  }
 
-  }, [])
+  const handleLogout = () => {
+    localStorage.removeItem("userData");
+    router.push("/")
+    getUserData()
+  };
 
   return (
     <header id="header">
@@ -140,12 +169,41 @@ export default function Navbar() {
                 <div
                   className="d-xl-flex d-none justify-content-center align-items-center gap-3"
                 >
-                  <Link
-                    href="/login"
-                    className="animated-btn"
-                  >
-                    Login
-                  </Link>
+                  <div className="dropdown">
+                    {
+                      userData ?
+                        <Dropdown align="end">
+                          <Dropdown.Toggle id="dropdown-basic" className="profile-dropdown">
+                            <Avatar url={undefined} />
+                          </Dropdown.Toggle>
+
+                          <Dropdown.Menu>
+                            <Link className="dropdown-item" href={userData?.role === "user" ? "/dashboard": "/dashboard/admin"}>
+                              <span className="icon me-2">
+                                <MdOutlineSpaceDashboard />
+                              </span>
+                              Dashboard
+                            </Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item as="button" onClick={handleLogout} className="logout-btn">
+                              <span className="icon me-2">
+                                <AiOutlineLogout />
+                              </span>
+                              Logout
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+
+                        : <Link
+                          href="/login"
+                          className="animated-btn"
+                        >
+                          Login
+                        </Link>
+                    }
+                  </div>
+
+
                 </div>
               </div>
             </div>
