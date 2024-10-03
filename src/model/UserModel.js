620ce56aa081a190151795eea5220bc86ba84126
Naim Ahmad <naimahmad201103@gmail.com>
2024-10-03 19:30:16 +0600

    crated user model

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -5,5 +6,17 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, default: "user" },
 });
+
+userSchema.pre("save", async function (next) {
+  // Check if password is modified
+  if (!this.isModified("password")) return next();
+
+  // If modified, hash the password
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt);
+
+  // Proceed to the next middleware
+  next();
+})
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
