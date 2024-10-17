c9b1721a478e4b4f386ec8cbce9797517990bcbb
Naim Ahmad <naimahmad201103@gmail.com>
2024-10-17 10:37:43 +0600

    fixed check api

--- src/app/api/auth/check/route.js
+++ src/app/api/auth/check/route.js
@@ -1,12 +1,16 @@
 import dbConnect from "@/lib/mongoose/dbConnect";
 import UserModel from "@/model/UserModel";
 
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 async function emailExistsInDatabase(email) {
-  await dbConnect()
+  await dbConnect();
   const user = await UserModel.findOne({ email });
-  return Boolean(user);
+  if (user && user?.password) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export async function GET(req) {
@@ -14,21 +18,27 @@ export async function GET(req) {
   const email = nextUrl.searchParams.get("email");
 
   if (!email) {
-    return NextResponse.json({
-      message: "Please provide an email address!"
-    }, {
-      status: 400,
-    });
+    return NextResponse.json(
+      {
+        message: "Please provide an email address!",
+      },
+      {
+        status: 400,
+      }
+    );
   }
 
   // Validate email format
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-    return NextResponse.json({
-      message: "Invalid email format!"
-    }, {
-      status: 400,
-    });
+    return NextResponse.json(
+      {
+        message: "Invalid email format!",
+      },
+      {
+        status: 400,
+      }
+    );
   }
 
   try {
@@ -37,9 +47,12 @@ export async function GET(req) {
 
     return NextResponse.json({ message: "Success", emailExists });
   } catch (error) {
-    return NextResponse.json({
-      message: "An error occurred while checking the email.",
-      error: error.message,
-    }, { status: 500 });
+    return NextResponse.json(
+      {
+        message: "An error occurred while checking the email.",
+        error: error.message,
+      },
+      { status: 500 }
+    );
   }
-};
+}
